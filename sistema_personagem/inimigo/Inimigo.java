00933ea8e9cf5808bd4ea66db36d870777c6d256
VE7OR <129470061+VE7OR@users.noreply.github.com>
2026-04-26 19:59:43 -0300

    Add Inimigo class with attributes and methods

--- sistema_personagem/inimigo/Inimigo.java
+++ sistema_personagem/inimigo/Inimigo.java
@@ -1,1 +1,29 @@
+package inimigo;
 
+public class Inimigo {
+    private int vida;
+    private int defesa;
+    private int defesaMagica;
+
+    public Inimigo(int vida, int defesa, int defesaMagica){
+        this.vida = vida;
+        this.defesa = defesa;
+        this.defesaMagica = defesaMagica;
+    }
+
+    public int getVida(){ return vida; }
+    public void setVida(int vida){ this.vida = vida; }
+
+    public int getDefesa(){ return defesa; }
+
+    public String getTipo(){
+        return "Inimigo";
+    }
+
+    public void mostrarStatus(){
+        System.out.println("Tipo: " + getTipo());
+        System.out.println("Vida: " + vida);
+        System.out.println("Defesa: " + defesa);
+        System.out.println("Defesa Magica: " + defesaMagica);
+    }
+}
