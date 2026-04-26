fab34552ef715f2501f53d80b67107f2a87a5299
VE7OR <129470061+VE7OR@users.noreply.github.com>
2026-04-26 20:02:18 -0300

    Add energy attribute and update Personagem class

--- sistema_personagem/personagem/Personagem.java
+++ sistema_personagem/personagem/Personagem.java
@@ -4,41 +4,39 @@ public class Personagem {
     private int vida;
     private int defesa;
     private int defesaMagica;
+    private int energia;
 
-    public Personagem(int vida, int defesa, int defesaMagica){
+    public Personagem(int vida, int defesa, int defesaMagica, int energia){
         this.vida = vida;
         this.defesa = defesa;
         this.defesaMagica = defesaMagica;
+        this.energia = energia;
     }
 
-    public int getVida() {
-        return vida;
+    public int getVida() { return vida; }
+    public int getDefesa() { return defesa; }
+    public int getEnergia() { return energia; }
+
+    public void setVida(int vida) { this.vida = vida; }
+    public void setEnergia(int energia) { this.energia = energia; }
+
+    public String getTipo(){
+        return "Personagem";
     }
 
-    public void setVida(int vida) {
-        this.vida = vida;
-    }
-
-    public int getDefesa() {
-        return defesa;
-    }
-
-    public void setDefesa(int defesa) {
-        this.defesa = defesa;
-    }
-
-    public int getDefesaMagica() {
-        return defesaMagica;
-    }
-
-    public void setDefesaMagica(int defesaMagica) {
-        this.defesaMagica = defesaMagica;
-    }
-
-    public void mostraDados(){
+    public void mostrarStatus(){
+        System.out.println("Tipo: " + getTipo());
         System.out.println("Vida: " + vida);
         System.out.println("Defesa: " + defesa);
-        System.out.println("Defesa Mágica: " + defesaMagica);
-        System.out.println("=====================");
+        System.out.println("Defesa Magica: " + defesaMagica);
+        System.out.println("Energia: " + energia);
+    }
+
+    public int atacar(){
+        return 10;
+    }
+
+    public int habilidadeEspecial(){
+        return atacar();
     }
 }
