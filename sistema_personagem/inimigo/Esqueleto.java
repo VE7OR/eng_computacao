533e540775214e77bd46fd6cf357821d62d166a5
VE7OR <129470061+VE7OR@users.noreply.github.com>
2026-04-28 12:20:10 -0300

    Adicionando descrição

--- sistema_personagem/inimigo/Esqueleto.java
+++ sistema_personagem/inimigo/Esqueleto.java
@@ -1,22 +1,42 @@
 package inimigo;
 
+// Classe Esqueleto herda (extends) da classe Inimigo
 public class Esqueleto extends Inimigo {
+
+    // Atributo específico do Esqueleto
     private int resistenciaOssea;
 
+    // Construtor da classe Esqueleto
     public Esqueleto(int vida, int defesa, int defesaMagica, int resistencia){
+
+        // Chama o construtor da classe pai (Inimigo)
+        // Inicializa vida, defesa e defesa mágica
         super(vida, defesa, defesaMagica);
+
+        // Inicializa o atributo específico do Esqueleto
         this.resistenciaOssea = resistencia;
     }
 
+    // Sobrescreve o método getTipo da classe Inimigo
     @Override
     public String getTipo(){
+
+        // Retorna o tipo do inimigo
         return "Esqueleto";
     }
 
+    // Sobrescreve o método mostrarStatus
     @Override
     public void mostrarStatus(){
+
+        // Mostra o nome do inimigo
         System.out.println("=== Esqueleto ===");
+
+        // Chama o método da classe pai para mostrar atributos básicos
+        // (vida, defesa, defesa mágica)
         super.mostrarStatus();
+
+        // Mostra o atributo exclusivo do Esqueleto
         System.out.println("Resistência Óssea: " + resistenciaOssea);
     }
 }
