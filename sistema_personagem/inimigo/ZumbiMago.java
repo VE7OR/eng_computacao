791c5ffacc5b78afb539db91092ebe22cd5aff31
VE7OR <129470061+VE7OR@users.noreply.github.com>
2026-04-28 12:26:35 -0300

    Adicionado descrição

--- sistema_personagem/inimigo/ZumbiMago.java
+++ sistema_personagem/inimigo/ZumbiMago.java
@@ -1,22 +1,41 @@
 package inimigo;
 
+// Classe ZumbiMago herda da classe Inimigo
 public class ZumbiMago extends Inimigo {
+
+    // Atributo exclusivo do Zumbi Mago
     private int magiaSombria;
 
+    // Construtor da classe ZumbiMago
     public ZumbiMago(int vida, int defesa, int defesaMagica, int magia){
+
+        // Chama o construtor da classe pai (Inimigo)
+        // Inicializa os atributos básicos
         super(vida, defesa, defesaMagica);
+
+        // Inicializa o atributo específico
         this.magiaSombria = magia;
     }
 
+    // Sobrescreve o método getTipo
     @Override
     public String getTipo(){
+
+        // Retorna o nome específico do inimigo
         return "Zumbi Mago";
     }
 
+    // Sobrescreve o método mostrarStatus
     @Override
     public void mostrarStatus(){
+
+        // Mostra o título do inimigo
         System.out.println("=== Zumbi Mago ===");
+
+        // Reaproveita o método da classe pai (vida, defesa, defesa mágica)
         super.mostrarStatus();
+
+        // Mostra o atributo exclusivo
         System.out.println("Magia Sombria: " + magiaSombria);
     }
 }
